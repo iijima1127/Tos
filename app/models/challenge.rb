db9a1f318f08e66bfa430b6eea07bb546475d95d
masaki.iijima <canchomeeen@gmail.com>
2020-07-05 04:55:50 +0000

    fixed footer users usershow

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,5 +2,5 @@ class Challenge < ApplicationRecord
   belongs_to :user
   validates :challenge, presence: true, length: {maximum: 255 }
   validates :goal, length: { maximum: 255 }
-  has_many :episodes, inverse_of: :challenge
+  has_many :episodes, inverse_of: :challenge, dependent: :destroy
 end
