cca9c2db8b74bd33d838f92b1b9816fe66bfc398
masaki.iijima <canchomeeen@gmail.com>
2020-07-25 14:17:29 +0000

    ユーザープロフィール画像と投稿画像・動画のバリデーション追加
    容量圧迫を防ぐため。

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -8,4 +8,41 @@ class Episode < ApplicationRecord
   belongs_to :challenge
   validates :content, presence: true, length: {maximum: 255 }
   validates :variation, presence: true, length: { maximum: 160 }
+  # 画像/動画投稿時のバリデーション対応のため。↓
+  validate :validate_image
+  validate :validate_clip
+  
+  # 画像/動画投稿時のバリデーション対応のため。↓
+    def validate_image
+      return unless images.attached? 
+      if images.blob.byte_size > 10.megabytes
+        images.purge 
+        errors.add(:images, '投稿可能な画像サイズ10MBまでです')
+      elsif !image?
+        images.purge
+        errors.add(:images, '投稿可能な画像データは「jpg、jpeg、gif、png」です。')
+      end
+    end
+    
+    def validate_clip
+      return unless clip.attached? 
+      if clip.blob.byte_size > 512.megabytes
+        clip.purge 
+        errors.add(:clip, '投稿可能な動画サイズ512MBまでです')
+      elsif !video?
+        clip.purge
+        errors.add(:clip, '投稿可能な動画データは「mp4、mov、qt」です。')
+      end
+    end
+    
+    private
+
+    def image?
+      %w[image/jpg image/jpeg image/gif image/png].include?(images.blob.content_type)
+    end
+    
+    def video?
+      %w[video/mp4 video/quicktime].include?(clip.blob.content_type)
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                       uniqueness: { case_sensitive: false }
     validates :profile, length: { maximum: 160 }
+    # 投稿画像のバリデーション対応のため↓
+    validate :validate_avatar
+
     has_secure_password
     has_one_attached :avatar
     has_many :challenges, inverse_of: :user, dependent: :destroy
@@ -13,6 +16,7 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: 'influence_id',dependent: :destroy
     has_many :influencers, through: :reverses_of_relationship, source: :user
     
+
     def influence(other_user)
       unless self == other_user
         self.relationships.find_or_create_by(influence_id: other_user.id)
@@ -26,5 +30,23 @@ class User < ApplicationRecord
     
     def influencing?(other_user)
       self.influences.include?(other_user)
+    end
+    
+    # 画像投稿時のバリデーション対応のため。↓
+    def validate_avatar
+      return unless avatar.attached? 
+      if avatar.blob.byte_size > 10.megabytes
+        avatar.purge 
+        errors.add(:avatar, '投稿可能な画像サイズ10MBまでです')
+      elsif !image?
+        avatar.purge
+        errors.add(:avatar, '投稿可能な画像データは「jpg、jpeg、gif、png」です。')
+      end
+    end
+    
+    private
+
+    def image?
+      %w[image/jpg image/jpeg image/gif image/png].include?(avatar.blob.content_type)
     end
 end
